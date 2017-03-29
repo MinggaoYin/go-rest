e6fcbefddf0429d6fb95ad4be986398171a0a64d
jacky.yin <jacky.yin@chaatz.com>
2017-03-29 17:27:27 +0800

    Create handlers dir

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"go-rest/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", Welcome)
+	http.HandleFunc("/", handlers.Welcome)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func Welcome(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world!\n"))
 }
